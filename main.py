791bb88f44a6a9d0fa49acc5fbb121c170a28d74
Lukáš Tyrychtr <lukastyrychtr@gmail.com>
2025-08-27 21:06:48 +0200

    Commits from bots are not interesting

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ def signature_time_str(signature):
     return time.strftime("%c %z")
 
 def commit_is_interesting(commit):
+    if "[bot]" in commit.author.name:
+        return False
     first_line = commit.message.splitlines()[0].lower()
-    no_interesting_prefixes = ["merge ", "bump ", "[tx-robot]", "merge!", "land #", "auto merge", "update dependency", "update rust crate", "rollup merge", "build(deps)", "automatic module_metadata_base.json update", "autosync the updated", "localisation update", "translated using", "chore(deps)", "[superproject]", "[gndoc]", "build(deps-dev)", "updating submodules", "autoformatter changes", "fix(l10n): update", "Update deployed packages for", "[chore]: Bump", "[Snyk]", "fix(deps)", "chore(assets): Recompile assets", "chore: Compile assets", "[deps]", "chore(deps-dev)"]
+    no_interesting_prefixes = ["merge ", "bump ", "[tx-robot]", "merge!", "land #", "auto merge", "update dependency", "update rust crate", "rollup merge", "build(deps)", "automatic module_metadata_base.json update", "autosync the updated", "localisation update", "translated using", "chore(deps)", "[superproject]", "[gndoc]", "build(deps-dev)", "updating submodules", "autoformatter changes", "fix(l10n): update", "Update deployed packages for", "[chore]: Bump", "[Snyk]", "fix(deps)", "chore(assets): Recompile assets", "chore: Compile assets", "[deps]", "chore(deps-dev)", "fix(translation)["]
     if "🔠" in first_line or "🛠️" in first_line:
         return False
     for prefix in no_interesting_prefixes:
